c4e81d32527833f65520bb2d699f686f270d8743
ChrisDrobnicky <krizzztofer@yahoo.com>
2017-07-12 20:24:45 +0200

    change header styling

--- src/components/Header/Header.component.js
+++ src/components/Header/Header.component.js
@@ -5,10 +5,10 @@ import styles from './Header.stylesheet.css';
 const Header = () => (
   <header className={styles.Header}>
     <nav className={styles.navWrapper}>
-      <div className={styles.appLogo}>
+      <h1 className={styles.appHeading}>
         Movie Picker
         <i className="film big icon"> </i>
-      </div>
+      </h1>
       <ul className={styles.navList}>
         <li className={styles.navListItem}><a href="#" className={styles.navLink}>Search Movies</a></li>
         <li className={styles.navListItem}><a href="#" className={styles.navLink}>Your Movies</a></li>
